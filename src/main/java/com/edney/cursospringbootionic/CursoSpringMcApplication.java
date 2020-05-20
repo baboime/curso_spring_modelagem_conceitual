d9be3df43369f8d33bdd3fe7f5cf7d8469b88467
Edney Baboim <ednney@gmail.com>
2020-05-20 11:25:20 -0300

    Projeto Criado

---
+++ src/main/java/com/edney/cursospringbootionic/CursoSpringMcApplication.java
@@ -0,0 +1,13 @@
+package com.edney.cursospringbootionic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringMcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringMcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edney/cursospringbootionic/CursoSpringMcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edney.cursospringbootionic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringMcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
