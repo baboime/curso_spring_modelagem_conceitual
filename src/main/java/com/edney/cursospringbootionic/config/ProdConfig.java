253f1cdb68f54f3cb6c1ddb0c77fe1de1c956d76
Edney Baboim <ednney@gmail.com>
2020-09-05 16:48:28 -0300

    Ajustes

---
+++ src/main/java/com/edney/cursospringbootionic/config/ProdConfig.java
@@ -0,0 +1,39 @@
+package com.edney.cursospringbootionic.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.edney.cursospringbootionic.servicos.ServicoDB;
+import com.edney.cursospringbootionic.servicos.ServicoEmail;
+import com.edney.cursospringbootionic.servicos.ServicoEmailSmtp;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+	
+	@Autowired
+	private ServicoDB servicoDB;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String estrategia;
+	
+	@Bean
+	public boolean instanciarBancoDeDados() throws ParseException {
+		
+		if (!"create".equals(estrategia)) {
+			return false;
+		}
+		servicoDB.instanciarBandoDeDadosDeTestes();
+		return true;
+	}
+	
+	@Bean
+	public ServicoEmail servicoEmail() {
+		return new ServicoEmailSmtp();
+	}
+}
