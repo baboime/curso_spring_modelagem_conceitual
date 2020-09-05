e1e51b9d3ef272ea5043189632cd1d9c6ccebf7f
Edney Baboim <ednney@gmail.com>
2020-09-05 17:58:50 -0300

    Documentacao com Swagger - Parte 1

--- src/main/java/com/edney/cursospringbootionic/config/SegurancaConfig.java
+++ src/main/java/com/edney/cursospringbootionic/config/SegurancaConfig.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -51,6 +52,15 @@ public class SegurancaConfig extends WebSecurityConfigurerAdapter {
 			"/clientes",
 			"/autorizacao/esqueci/**"
 	};
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/v2/api-docs/**");
+	    web.ignoring().antMatchers("/swagger.json");
+	    web.ignoring().antMatchers("/swagger-ui.html");
+	    web.ignoring().antMatchers("/swagger-resources/**");
+	    web.ignoring().antMatchers("/webjars/**");
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
